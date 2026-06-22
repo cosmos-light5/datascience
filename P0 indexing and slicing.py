ec9f4f3bb74525d6081d6da82b3a43d73adf5a78
Cosmos_light5 <thapa48prakash@gmail.com>
2026-06-22 11:54:32 +0545

    data science initiating

---
+++ P0 indexing and slicing.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+tmp_data = [1.3, 1.4, 8.6,4.3,3.6,9.1,1.7,2.0,6.1,3.6,7.6,1.3,3.3,8.7,7.5,3.6,4.0,10.7,1.7,9.3,5.8,2.8,0.8]
+tmp_arr =np.array(tmp_data)
+
+
+print(tmp_arr.shape)
+print(tmp_arr.dtype)
+
+print(f"Temperature at 12PM is {tmp_arr[11]}")
+
+
+print(f"Temperature at 12PM is {tmp_arr[11]}")
