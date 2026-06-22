50ab2466fad1c3b997feb60a434b13e89053659f
Cosmos_light5 <thapa48prakash@gmail.com>
2026-06-22 15:22:38 +0545

    displaying array, shape and type of q1.

---
+++ Vectorized Operation01.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+cost_prices = [100, 150, 200, 250, 300]
+
+cp_ar = np.array(cost_prices)
+
+#1st soln:
+print(cp_ar)
+print(cp_ar.shape)
+print(cp_ar.dtype)              #int64--> 64bits ma chaleko xa
+
