c6627e222152836cd365d9e58f183870fce71b9d
Cosmos_light5 <thapa48prakash@gmail.com>
2026-06-24 15:31:22 +0545

    filtering odd num

--- Data Transformation01.py
+++ Data Transformation01.py
@@ -2,13 +2,13 @@
 import numpy as np
 
 np.random.seed(5)
-my_arr = np.random.randint(5,50,(8,))
+my_arr = np.random.randint(5,50,(8,))          #(5,50,(8,)) -->5 dekhi 50 vitra ko data dinxa tara 8 wata matra   
 print(my_arr)
 """ Output: [40 19 43 21 14 13 41 44] """
 
 
 # filtering array greater than 20 from seeded random num. and resulting if the numbers meet the condition.
-filter_mask = my_arr > 20
+filter_mask = my_arr > 20           #compare garera corresponding indexes ma boolean array dinxa not exact values
 print(filter_mask)
 """ Output: [ True False  True  True False False  True  True] """
 
@@ -22,4 +22,9 @@ print(filter_result)
 filter_even_result = my_arr [my_arr % 2== 0]
 print(filter_even_result)
 """ Output:[40 14 44] """
+
+#filter odd num greater than 20
+filter_mask =  (my_arr % 2 != 0) & (my_arr > 20)      #& use garne "and" use gare ma error
+filter_odd_result = my_arr[filter_mask]
+print(filter_odd_result)
 
---
+++ Q.Transforming Arrays.py
@@ -0,0 +1,14 @@
+# Create 4x4 array with values randomly from 0-255. Reshape it to 3x3 with np.resize.
+
+
+# Create 6x6 array with values randomly from 0-255. Reshape it to 4x9 with np.shape.
+
+
+# Create 4x8 array from normal distribution (u=40, sd=10). Transpose this and print.
+
+
+# Create 3x3 array with random value from 0-255. Convert it to 4x4 such that excess element to left and top are filled with zero.
+
+
+# Create 4x4 array with random values from 0-20. Convert it into 1D using reshape, resize, flatten and ravel.
+
