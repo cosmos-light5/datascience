4485e2f2f8f89174cb39f006082fbe6f24c362d7
Cosmos_light5 <thapa48prakash@gmail.com>
2026-06-24 15:05:47 +0545

    filtering array greater than 20 from seeded random num. in terms of T/F

--- Data Transformation01.py
+++ Data Transformation01.py
@@ -6,5 +6,11 @@ my_arr = np.random.randint(5,50,(8,))
 print(my_arr)
 
 
+# filtering array greater than 20 from seeded random num. and resulting if the numbers meet the condition.
 filter_mask = my_arr > 20
 print(filter_mask)
+
+
+# filtering array greater than 20 from seeded random num.
+filter_result = my_arr [filter_mask]        # alternet way--> my_arr [my_arr > 20]
+print(filter_result)
---
+++ Q.Conditional (where, select).py
@@ -0,0 +1,13 @@
+# Write a function incrment_salary that takes salary_array and returns increased salary. Hike Rule: <1000 => 20%, 1000-5000 => 10%, >5000 => 5%. (use: np.select)
+
+
+# Write a function categorize_age that takes age_array and categorizes it as Child, Teen, Adult, Senior depending on age range. (use: np.select)
+
+
+# Assign temp_data as [15, 22, 30, 5, 18, 25]. Generate new array temp_category that has elements below_avg and above_avg. (use: np.where)
+
+
+
+
+# Assign player_name as ["Player1", "Player2", "Player3", "Player4"], player_score as [85, 92, 78, 90]. Find the name of players with score above 90 and store as top_players.
+
