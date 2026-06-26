61b944ad7d9d2b4685be04f9ed31dd81defc2b25
Cosmos_light5 <thapa48prakash@gmail.com>
2026-06-26 14:21:41 +0545

    copy array

--- Data Transformation02.py
+++ Data Transformation02.py
@@ -76,5 +76,30 @@ print()
 # two matrix ma kunai col common huna sakxa so intersecting col eakpatak ra remaining lai add garna axis 1
 combined_data = np.concatenate([rushter_reading,jairaj_reading], axis=1)
 print(combined_data)
+""" Output: 
+[[ 1.2 22.   1.2  0.1]
+ [ 0.3 20.   5.2  0.6]
+ [ 1.5 21.   2.7  0.7]]"""
+print()
+
+#alternative way for axis=1
+combined_data = np.hstack([rushter_reading,jairaj_reading])     #hstack(())--> row ko lagi 7.30; horizontal stack
+print(combined_data)
+""" Output: 
+[[ 1.2 22.   1.2  0.1]
+ [ 0.3 20.   5.2  0.6]
+ [ 1.5 21.   2.7  0.7]]"""
+print()
+
+combined_data = np.vstack([rushter_reading,jairaj_reading])
+print(combined_data)
+""" Output: #alternative way for axis=1"""
+print()
+
+arr_1=np.array([2,3,8])
+arr_2=arr_1.copy()              #arr_2=arr_1 matra vayeko vaye arr_1 ra 2 print garda same result hunthyo i.e. eutai manxeko duita nam jasto
+arr_1[2]=4
+print(arr_1)
+print(arr_2)
 """ Output: """
 print()
